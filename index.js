bd3cc8dc3b9474aaca9471eaf8268ecc707385c5
Jason Walsh <jason.walsh@memorylane.com>
2011-07-27 20:40:17 -0700

    Showing running port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
-  connect = require('connect'),
-  server;
+connect = require('connect'),
+server,
+PORT = 8080;
 
 server = connect.createServer();
 server.use(connect.profiler());
@@ -8,4 +9,5 @@ server.use(connect.bodyParser());
 server.use(connect.cookieParser());
 server.use(connect.static(__dirname + '/public', { maxAge: 999999 }));
 server.use(connect.errorHandler({ stack: true, dump: true }));
-server.listen(8080);
+server.listen(PORT);
+console.log("Running http://localhost:" + PORT + "/");
