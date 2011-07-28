18ebb9cdb7e16dda0ac2136966c16e1f21fe5b10
Li <jonli@70cd605a3b5a.(none)>
2011-07-27 20:29:07 -0700

    add static handler and stub html

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var fs = require('fs'),
+  connect = require('connect'),
+  server;
 
-module.exports = require('./lib/node-kyn');
+server = connect.createServer();
+server.use(connect.profiler());
+server.use(connect.bodyParser());
+server.use(connect.cookieParser());
+server.use(connect.static(__dirname + '/public', { maxAge: 999999 }));
+server.use(connect.errorHandler({ stack: true, dump: true }));
+server.listen(8080);
