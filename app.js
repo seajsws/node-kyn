da4097f89e676b352db58991ecd5414d24728afb
Colin May <satsukikorin@gmail.com>
2011-08-31 20:17:33 -0700

    Hello world

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var express = require('express');
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Know Your Neighbor');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
-});
